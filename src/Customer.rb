e49875ef099824774a254b423e94d79e369e5e7b
Kfoster14 <kelleyafoster@hotmail.com>
2020-12-15 21:04:10 +1000

    11th commit from master_modified customer and travelstore files

--- src/Customer.rb
+++ src/Customer.rb
@@ -1,51 +1,35 @@
 # For the Adventure Travel App, the following classes are needed: Customer, Activities, City, Booking, Cancellation
-# Customer attributes: first name, last name, home address, email address
+# Customer attributes: first name, last name, home address, email address, customer type
 # Customer actions are: view activity list, book an activity, cancel an activity, exit app
+
+require_relative("./travelstore.rb")
+
 @@booking_number = 0
 class Customer
-    attr_reader :first_name, :last_name, :home_address, :email_address, :customer_type 
+    attr_reader first_name, last_name, home_address, email_address, customer_type 
     def initialize(customer_details)
-        @first_name = customer_details[:first_name]
-        @last_name = customer_details[:last_name]
-        @home_address = customer_details[:home_address]
-        @email_address = customer_details[:email_address]
-        @customer_type = customer_details[:customer_type]
+        @first_name = first_name
+        @last_name = last_name
+        @home_address = home_address
+        @email_address = email_address
+        @customer_type = customer_type
         @activities_array = []
-        @special_activities_array = []
         @@booking_number += 1
-    
-    
-    
-    def view_activities_list()
-        
     end
+    
+    puts "List of activities"
+    activities = [{"activity_name" => "Harbour Bridge Climb", "date" => "21st December 2020"}, {"activity_name" => "Rollercoaster zip line", "date" => "31st December 2020", "availabilty" => "Sold Out!"},
+                {"activity_name" => "Rainforest Canyon Adventure", "date" => "21st December 2020"}, {"activity_name" => "Surfing at Bondi", "date" => "21st January 2021"}, {"activity_name" => "Hiking in the Blue Mountains", "date" => "14th February 2021"}, 
+                {"activity_name" => "Parasailing in Manly", "date" => "2nd March 2021"}, {"activity_name" => "Hot Air Ballooning", "date" => "2nd January 2021"}]
 
-    def enter_details
-        require "tty-prompt"
-        prompt = TTY::Prompt.new
-        prompt.yes?("Would you like to book an adventure?")
-        if prompt.yes? == true
-            customer_details = prompt.collect do
-                key(:first_name).ask("What is your first name")
-          
-                key(:last_name).ask("What is your last name")
-          
-                key(:home_address) do
-                    key(:street).ask("Please enter your street name", required: true)
-                    key(:city).ask("Please enter your suburb")
-                    key(:postcode).ask("Please enter your post code", validate: /\A\d{3}\Z/)
-                end
-            end
-            prompt.ask("What is your email address?") do |q|
-                q.validate(/\A\w+@\w+\.\w+\Z/, "Invalid email address")
-              end
-        else 
-      end
-      p enter_details
-
-   
+    result = activities.map {|x| x.values}
+    result2 = result.join(", ")
+    p result2
+    
     def book_activity(activity_name)
-        @activities_array << {activity: activity_name}
+        puts "Would you like to book an activity? (yes/no)" 
+        input = gets.chomp    
+        @activities_array << activity_name
         puts "Thank you #{@first_name}. The details of your chosen activity are as follows: #{@activities_array}."
         puts "Your booking has been confirmed. Your booking number is #{@@booking_number}"
     end
@@ -54,7 +38,7 @@ class Customer
         puts "Are you sure you want to cancel your booking? (yes/no)"
         answer = gets.chomp
         puts answer
-        if answer = "yes"
+        if answer == "yes"
             puts "Please enter your booking ID number."
             booking_number = gets.chomp.to_i
             puts booking_number
@@ -70,10 +54,13 @@ class Customer
 
     def to_s
         customer_message = "Welcome to the Adventure Travel App #{@first_name}!"
-        latest_activity = "The latest addition to our activities is "
+        latest_activity = "The latest addition to our activities is skydiving"
         return customer_message + latest_activity
     end
-
 end
 
-non_member = Customer.new()
+Customer.new()
+    
+     
+    
+    
